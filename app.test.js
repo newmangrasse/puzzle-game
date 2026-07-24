e32045d4e1885d1e809719c3f08835b4d83b961f
newmangrasse <grasseman648@gmail.com>
2026-07-24 18:32:58 +0545

    Create app.test.js
    new again

---
+++ app.test.js
@@ -0,0 +1,14 @@
+// app.test.js - Tests for our calculator
+const { add, subtract, multiply } = require('./app');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('multiplies 3 * 4 to equal 12', () => {
+  expect(multiply(3, 4)).toBe(12);
+});
