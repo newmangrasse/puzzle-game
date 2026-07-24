103d182d183d8ca9cd92ecd4ebd08ace0b612ced
newmangrasse <grasseman648@gmail.com>
2026-07-24 18:32:06 +0545

    Create app.js
    new file for workflow test

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js - A simple calculator app
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, subtract, multiply };
+
