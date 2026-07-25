ec472651ac749ba03ced5644d06f483414357945
newmangrasse <grasseman648@gmail.com>
2026-07-25 13:27:38 +0545

    Create random.js
    commiting new oen

---
+++ random.js
@@ -0,0 +1,4 @@
+// Hardcoded AWS secret key (fake but looks real)
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const DB_PASSWORD = "P@ssw0rd123!";
+console.log("This is a vulnerable app");
