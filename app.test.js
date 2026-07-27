521c6c334f90c837aa119461217cbc04c14dfa9c
newmangrasse <grasseman648@gmail.com>
2026-07-27 22:40:58 +0545

    Update app.test.js
    chnges

--- app.test.js
+++ app.test.js
@@ -8,7 +8,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('subtracts 5 - 2 to equal 3', () => {
   expect(subtract(5, 2)).toBe(3);
 });
-
+//aaa
 test('multiplies 3 * 4 to equal 12', () => {
   expect(multiply(3, 4)).toBe(12);
 });
